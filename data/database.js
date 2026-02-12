5477bbcc8700881b8edd3c6e29c87e124b44711e
Henrique-de-jesus <henriquedejesusca@gmail.com>
2026-02-12 11:11:10 -0300

    "database" atualizado!

---
+++ data/database.js
@@ -0,0 +1,122 @@
+//Usuario
+export const user = [{
+    id: 1,
+    nome: "usuario",
+    email: "teste@teste.com",
+    senha: "123",
+    tipo: "user"
+}];
+[{
+    id: 2,
+    nome: "administrador",
+    email: "Admin@teste.com",
+    senha: "321",
+    tipo: "Admin"
+
+}];
+export const products = [{
+    id : 1,
+    nome: "Salsicha",
+    descricao: "Enroladinho de salsicha",
+    preco:  2.50,
+    estoque: 5,
+    categoria: "Salgado"
+}];
+[{
+    id : 2,
+    nome: "Coxinha",
+    descricao: "Coxinha de frango",
+    preco:  2.50,
+    estoque: 5,
+    categoria: "Salgado"
+}];
+[{
+    id : 3,
+    nome: "Pastel",
+    descricao: "Pastel de carne",
+    preco:  3.50,
+    estoque: 5,
+    categoria: "Salgado"
+
+}];
+[{
+    id : 4,
+    nome: "Mini Pizza",
+    descricao: "Mini pizza de carne",
+    preco:  6.00,
+    estoque: 5,
+    categoria: "Salgado"
+
+}];
+[{
+    id : 5,
+    nome: "Folhado de carne",
+    descricao: "Assado folhado de carne",
+    preco:  5.00,
+    estoque: 5,
+    categoria: "Salgado"
+
+}];
+[{
+    id : 6,
+    nome: "Folhado de queijo",
+    descricao: "Assado folhado de queijo",
+    preco:  5.00,
+    estoque: 5,
+    categoria: "Salgado"
+
+}];
+[{
+    id : 7,
+    nome: "Dogão",
+    descricao: "Cachorro-quente",
+    preco:  7.00,
+    estoque: 5,
+    categoria: "Salgado"
+
+}];
+[{
+    id : 8,
+    nome: "Assado de Calabresa",
+    descricao: "salgado de calabresa",
+    preco:  5.00,
+    estoque: 5,
+    categoria: "Salgado"
+
+}];
+[{
+    id : 9,
+    nome: "Soda",
+    descricao: "Refrigerante de limão",
+    preco:  3.49,
+    estoque: 4,
+    categoria: "Refrigerante"
+
+}];
+[{
+    id : 10,
+    nome: "Coca Cola",
+    descricao: "refrigerante de cola",
+    preco:  2.59,
+    estoque: 4,
+    categoria: "Refrigerante"
+
+}];
+[{
+    id : 11,
+    nome: "Fanta de laranja",
+    descricao: "Refrigerante de laranja",
+    preco:  3.49,
+    estoque: 4,
+    categoria: "Refrigerante"
+
+}];
+[{
+    id : 12,
+    nome: "Guaraná",
+    descricao: "Refrigerante de guaraná",
+    preco:  2.59,
+    estoque: 4,
+    categoria: "Refrigerante"
+
+}];
