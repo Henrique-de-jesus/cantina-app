7c37bd9bd4e3de23e28bcfd70709521837887754
Arthur09324 <arthurguedert99@gmail.com>
2026-02-12 09:41:02 -0300

    login com cor pessego

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -30,7 +30,7 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fae1dd',
     alignItems: 'center',
     justifyContent: 'center',
   },
