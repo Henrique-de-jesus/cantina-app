b3676b120915d4062699557ca416f7e188bbb866
Jvz1N-br <inaciodasilvajoaovictor3@gmail.com>
2026-02-12 10:34:42 -0300

    correção mde erros

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -4,7 +4,7 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from "reac
 export default function HomeScreen() {
     return  (
         <View>
-            <Text style={styles.marca}>Bem Vindo!</Text>
+            <Text style={styles.marca}>Olá, ${usuario}</Text>
         </View>
     )
 }
