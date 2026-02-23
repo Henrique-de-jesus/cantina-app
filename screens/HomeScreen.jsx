5e2aa43fdcaaf4e1a6e28d5656d7687b301149e0
Henrique-de-jesus <henriquedejesusca@gmail.com>
2026-02-23 08:19:11 -0300

    tentando identificar e arrumar algum erro

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,44 +1,69 @@
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView } from "react-native";
 import { useEffect, useState } from "react";
-import {getProducts} from '../services/productService'
+import { getProducts } from '../services/productService'
 
 
 export default function HomeScreen() {
   const [products, setProducts] = useState([])
-  useEffect(()=>{
-    setProducts(getProducts())
-  },[])
-  return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.marca}>Bem vindo</Text>
-    
-      <View>
-    <TouchableOpacity>
-      <Image style={styles.imagem}
-        source={products[0].image} />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Image style={styles.imagem}
-        source={require('../image/doguinho.png')} />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Image style={styles.imagem}
-        source={require('../image/pastel.png')} />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Image style={styles.imagem}
-        source={require('../image/coca-cola.png')} />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Image style={styles.imagem}
-        source={require('../image/fantaL.png')} />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Image style={styles.imagem}
-        source={require('../image/guarana.png')} />
-    </TouchableOpacity>
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  async function fetchData() {
+    try {
+      const data = await getProducts();
+      setProducts(data)
+    }
+    catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+  if (loading){
+    return (
+      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>Carregando...</Text>
     </View>
-    
+    )
+  }else{
+
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.marca}>Bem vindo</Text>
+  
+        <View>
+            <TouchableOpacity>
+              <Image style={styles.imagem}
+                source={products[0].image} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.imagem}
+                source={require('../image/doguinho.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.imagem}
+                source={require('../image/pastel.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.imagem}
+                source={require('../image/coca-cola.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.imagem}
+                source={require('../image/fantaL.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.imagem}
+                source={require('../image/guarana.png')} />
+            </TouchableOpacity>
+      )
+  }
+
+      </View>
+
     </ScrollView>
   )
 }
@@ -58,5 +83,6 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     borderWidth: 1,
-    margin: 15  }
+    margin: 15
+  }
 })
